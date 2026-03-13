e6b759cf3a97d48d307b971f80b5bcf561805e66
C1F5D2DD <3238858647@qq.com>
2026-03-13 18:23:45 +0800

    main2

--- mian.py
+++ mian.py
@@ -2,4 +2,16 @@ from astrbot.api.event import filter, AstrMessageEvent, MessageEventResult
 from astrbot.api.star import Context, Star, register
 from astrbot.api import logger # 使用 astrbot 提供的 logger 接口
 
+
+print=logger.info
+
 class HideAIChatter(star):
+    def __init__(self, context: Context):
+        super().__init__(context)
+
+    @filter.on_decorating_result()
+    async def on_decorating_result(self, event: AstrMessageEvent):
+        result = event.get_result()
+        chain = result.chain
+        print(chain)  # 打印消息链
+        chain.append(Plain("!"))
