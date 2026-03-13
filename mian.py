6c2e9da561ca438a145721804ebc3db8248446ea
C1F5D2DD <3238858647@qq.com>
2026-03-13 18:16:54 +0800

    meta

---
+++ mian.py
@@ -0,0 +1,5 @@
+from astrbot.api.event import filter, AstrMessageEvent, MessageEventResult
+from astrbot.api.star import Context, Star, register
+from astrbot.api import logger # 使用 astrbot 提供的 logger 接口
+
+class HideAIChatter(star):
